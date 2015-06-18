73ed57aa8b4e0ef79c887034bef63acc98b16bd3
Ron Keidar <ron.keidar@verint.com>
2015-06-18 14:54:39 -0700

    change LAN name to "Ethernet" instead of "Local Area Network"

--- Scripts/Batch/FixtureStettings.bat
+++ Scripts/Batch/FixtureStettings.bat
@@ -1,9 +1,9 @@
-cmd /c  "mkdir c:\share  >c:\setting.log"
-cmd /c  "net share share=c:\share /GRANT:everyone,FULL >c:\share\setting.log"
-cmd /c "sc stop puppet  >>c:\share\setting.log"
-cmd /c "sc config puppet start= disabled >>c:\share\setting.log"
-cmd /c "rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log"
-cmd /c "netsh interface ip set dns "Ethernet" dhcp >>c:\share\setting.log"
-cmd /c "netsh interface ip set address "Etherent" dhcp >>c:\share\setting.log"
-ipconfig /all >>c:\share\setting.log"
+cmd /c  mkdir c:\share  >c:\setting.log
+cmd /c  net share share=c:\share /GRANT:everyone,FULL >>c:\share\setting.log
+cmd /c sc stop puppet  >>c:\share\setting.log
+cmd /c sc config puppet start= disabled >>c:\share\setting.log
+cmd /c  rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log
+cmd /c netsh interface ip set dns "Ethernet" dhcp >>c:\share\setting.log
+cmd /c netsh interface ip set address "Ethernet" dhcp >>c:\share\setting.log
+ipconfig /all >>c:\share\setting.log
 type c:\share\setting.log
