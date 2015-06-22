9e3718289c2f38e4d0c4af601e71cd4b64f3e4fb
Ron Keidar <ron.keidar@verint.com>
2015-06-22 15:23:53 -0700

    timeout

--- Scripts/Batch/FixtureStettings.bat
+++ Scripts/Batch/FixtureStettings.bat
@@ -1,6 +1,7 @@
 cmd /c  mkdir c:\share  >c:\setting.log
 cmd /c  net share share=c:\share /GRANT:everyone,FULL >>c:\share\setting.log
 cmd /c sc stop puppet  >>c:\share\setting.log
+timeout 15
 cmd /c sc config puppet start= disabled >>c:\share\setting.log
 cmd /c  rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log
 cmd /c netsh interface ip set dns "Ethernet" dhcp >>c:\share\setting.log
