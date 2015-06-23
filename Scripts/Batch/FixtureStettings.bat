4c7774cc62ddfc5bc20d614ce790ff5d22a4b46d
Ron Keidar <ron.keidar@verint.com>
2015-06-23 16:19:59 -0700

    groovy

--- Scripts/Batch/FixtureStettings.bat
+++ Scripts/Batch/FixtureStettings.bat
@@ -1,10 +1,11 @@
 cmd /c  mkdir c:\share  >c:\setting.log
 cmd /c  net share share=c:\share /GRANT:everyone,FULL >>c:\share\setting.log
 cmd /c sc stop puppet  >>c:\share\setting.log
-timeout 15 >>c:\share\setting.log
+cmd /c timeout 15 >>c:\share\setting.log
 cmd /c sc config puppet start= disabled >>c:\share\setting.log
 cmd /c  rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log
 cmd /c  rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log
 cmd /c netsh interface ip set dns "Ethernet" dhcp >>c:\share\setting.log
 cmd /c netsh interface ip set address "Ethernet" dhcp >>c:\share\setting.log
 cmd /c ipconfig /all >>c:\share\setting.log
+cmd /c taskkill /IM "PSEXESVC.exe" /F
