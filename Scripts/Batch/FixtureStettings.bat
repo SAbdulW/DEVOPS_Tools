d2edbc70e1a63b7b0166113de4514a895c3ff95d
Ron Keidar <ron.keidar@verint.com>
2017-03-08 15:55:54 -0800

    Merge branch 'ci_wfo_env_test' of atlgit:DEVOPS_Tools into ci_wfo_env_test
    Conflicts:
    	Scripts/InstallBuilder/build.gradle
    	Scripts/InstallBuilder/gradle.properties

--- Scripts/Batch/FixtureStettings.bat
+++ Scripts/Batch/FixtureStettings.bat
@@ -1,10 +1,25 @@
+echo delete c:\share
 cmd /c  mkdir c:\share  >c:\setting.log
+Echo net share share=c:\share /GRANT:everyone,FULL
 cmd /c  net share share=c:\share /GRANT:everyone,FULL >>c:\share\setting.log
+echo sc stop puppet 
 cmd /c sc stop puppet  >>c:\share\setting.log
+echo timeout 15
+cmd /c timeout 15 >>c:\share\setting.log
+echo sc config puppet start= disabled
 cmd /c sc config puppet start= disabled >>c:\share\setting.log
+echo rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl
 cmd /c  rd /S /Q C:\ProgramData\PuppetLabs\puppet\etc\ssl >>c:\share\setting.log
+set res=0
+ipconfig /all| findstr "DHCP Enabled. . . . . . . . . . . : Yes" 1>nul
+if errorlevel 1 set res=90
+echo netsh interface ip set dns "Local Area Connection" dhcp 
 cmd /c netsh interface ip set dns "Local Area Connection" dhcp >>c:\share\setting.log
+echo netsh interface ip set address "Local Area Connection" dhcp 
 cmd /c netsh interface ip set address "Local Area Connection" dhcp >>c:\share\setting.log
-
-
-
+echo ipconfig /all
+cmd /c ipconfig /all >>c:\share\setting.log
+echo taskkill /IM "PSEXESVC.exe" 
+cmd /c taskkill /IM "PSEXESVC.exe" /F
+echo Done!!!!!
+exit res
---
+++ Scripts/Groovy/nodeDepreciation.groovy
@@ -0,0 +1,21 @@
+import hudson.FilePath
+import jenkins.model.Jenkins
+import hudson.model.Node
+import hudson.model.Computer
+
+def sTempLabel = "swarm Depreciation-Slaves"
+def build = Thread.currentThread().executable
+def String sNodeName = build.getEnvironment(listener).get('MachineName')
+Jenkins jenkins = Jenkins.instance
+for (Node node in jenkins.nodes) {
+  def Computer comp = node.toComputer()
+  def String sDisplayName = comp.getDisplayName()
+  if (sDisplayName ==~ "${sNodeName}") {
+    println "Found node: ${sDisplayName}"
+    if ((node.getLabelString() != sTempLabel)) {
+      node.setLabelString(sTempLabel)
+      println "Node: ${sNodeName} has moved to pool: " + sTempLabel
+    }
+    break
+  }
+}
