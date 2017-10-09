ae26ddd666bc0cd243d11747690002383c9b1a5f
Hoai Tang <hoai.tang@verint.com>
2017-10-09 16:34:22 -0700

    Deploy EAR support getVersion - update function

--- Scripts/DeployEAR/util/GetInstalledVersionFromRegistry.ps1
+++ Scripts/DeployEAR/util/GetInstalledVersionFromRegistry.ps1
@@ -1,60 +1,74 @@
-param($vmIP, $jsonPath)
+param($vmIP, $vmUser="ap2admin", $vmPwd="verint1!", $componentName, $jsonOutFileName)
 . $PSScriptRoot\PSConstant.ps1
 . $PSScriptRoot\PSCommonLibs.ps1
 
 if ($error) { $error.clear() }
-$result = $false
+$resultVersion = ''
 $psLibs = New-Object CommonLibs
 $psCredential = $psLibs.createRemoteCredential($vmIP, $vmUser, $vmPwd)
-$fileName = $jsonPath.Split('\') | Select-Object -Last 1
-$remoteFolder = '\\' + $vmIP + '\C$\Impact360'
-if ($psLibs.disconnectNetworkMap($remoteFolder)) {
-  New-PSDrive -Name X -PSProvider FileSystem -Root $remoteFolder -Credential $psCredential
-}
-Copy-Item -Path $jsonPath -Destination 'X:\' -Force
-Write-Host "[GetInstalledVersionFromRegistry] - KB list json file copied!"
+
+Write-Host "[GetInstalledVersionFromRegistry] - Searching Version for:"$componentName
+
 $sbContent = {
-  param($jsonName)
-  $jsonFile = "C:\Impact360\$jsonName"
-  $jsonData = [System.IO.File]::ReadAllText($jsonFile) | ConvertFrom-Json
+  param($iComponentName)
+
+    
   $regkey64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
   $regkey32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+
+  Write-Host "[GetInstalledVersionFromRegistry] - Searching Version for:"$iComponentName
+
+  $key = Get-ChildItem $regkey64 -Recurse | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object {$_.DisplayName -eq $iComponentName}
   $foundKey = $true
-  ForEach ($item in $jsonData) {
+  $ComponentVersion = ''
 
-    Write-Host "[GetInstalledVersionFromRegistry] - Searching Version for:"$item.componentID
-    $key = Get-ChildItem $regkey64 -Recurse | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object {$_.DisplayName -eq $item.componentID}
-
+  if (!($key)) {
+    Write-Host "[GetInstalledVersionFromRegistry] - Not found"$iComponentName"in 64bit keys. Now Searching on 32bit keys..."
+    $key = Get-ChildItem $regkey32 -Recurse | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object {($_.DisplayName -match $iComponentName)}
+  
     if (!($key)) {
-        Write-Host "[GetInstalledVersionFromRegistry] - Not found"$itemArtifact.artifactId"in 64bit keys. Now Searching on 32bit keys..."
-        $key = Get-ChildItem $regkey32 -Recurse | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object {($_.DisplayName -match $item.componentID)}
-    }
-
-    if (!($key)) {
-        Write-Host "[GetInstalledVersionFromRegistry] - Not found"$item.componentID"in registry!"
+        Write-Host "[GetInstalledVersionFromRegistry] - Not found"$iComponentName"in registry!"     
         $foundKey = $false
-        break
-    }
-
-    $ComponentVersion = $key.DisplayVersion
-
-    break
-
+    }    
   }
-  Write-Host "[GetInstalledVersionFromRegistry] - Checking result on $vmIP : $foundKey"
-  $jsonObj = New-Object -TypeName PSObject
-  Add-Member -InputObject $jsonObj -NotePropertyName 'ComponentVersion' -NotePropertyValue $ComponentVersion.toString()
-  Add-Member -InputObject $jsonObj -NotePropertyName 'Result' -NotePropertyValue $foundKey.toString()
-  $jsonData = ConvertTo-Json -InputObject $jsonObj
-  Set-Content -Path $jsonFile -Value $jsonData
+  
+  if ($foundKey){
+        $ComponentVersion = $key.DisplayVersion
+  }
+    
+  Write-Host "[GetInstalledVersionFromRegistry] - Result finding $iComponentName : $ComponentVersion"
+  
+  return $ComponentVersion
 }
+
+#START EXECUTION
+
 if ($psLibs.addTrustedIP($vmIP)) { Get-Item WSMan:\localhost\Client\TrustedHosts }
+
 if ($psLibs.ensureRemoteCall($vmIP, $vmUser, $vmPwd)) {
-  Write-Host "[GetInstalledVersionFromRegistry] - Verify KB list from test machine registry ..."
-  Invoke-Command -Computer $vmIP -ScriptBlock $sbContent -ArgumentList $fileName -Credential $psCredential
+  Write-Host "[GetInstalledVersionFromRegistry] - invoking command remotely ..."
+  $resultVersion = Invoke-Command -Computer $vmIP -ScriptBlock $sbContent -ArgumentList "$componentName"  -Credential $psCredential
 }
+
+Write-Host "[GetInstalledVersionFromRegistry] - Result execution remotely $ComponentName : $resultVersion"
+
 if (-Not $psLibs.removeTrustedIP($vmIP)) {Clear-Item WSMan:\localhost\Client\TrustedHosts -Force}
-Move-Item -Path "X:\$fileName" -Destination $jsonPath -Force
-Remove-PSDrive -Name X -Force
+
+$jsonObj = New-Object -TypeName PSObject
+
+Add-Member -InputObject $jsonObj -NotePropertyName 'ComponentVersion' -NotePropertyValue $resultVersion.toString()
+
+$jsonData = ConvertTo-Json -InputObject $jsonObj
+
+Write-Host "[GetInstalledVersionFromRegistry] - Saving Version to:"$jsonOutFileName
+
+$jsonData | out-file -FilePath $jsonOutFileName -Encoding utf8 -Force
+
+#Set-Content -Path ".\$jsonOutFileName" -Value $jsonData
+
 
 if ($error) {Write-Host "ERROR: $error"}
+
+Write-Host "[GetInstalledVersionFromRegistry] - EOE $ComponentName : $resultVersion"
+
+return $resultVersion
